3eebc1fcc15bb5d2d6961cae5e6b904e6f462643
Bruno Brito <developer.bcbrito@gmail.com>
2022-09-29 21:28:53 -0300

    removido linguagens api do projeto e separado em outro repositorio

--- linguagens-api/src/main/java/br/com/bcbrito/linguagens/api/LinguagemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bcbrito.linguagens.api;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
-    
-}
